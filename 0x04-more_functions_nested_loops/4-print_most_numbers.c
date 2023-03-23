4f145a5ad44b70739a2d01ab90869a1434af5aa6
aissam <aissam@gmail.com>
2023-03-23 08:32:38 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_most_numbers- function
+ *Return: no zeturn
+ */
+void print_most_numbers(void)
+{
+	char c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+		{
+			continue;
+		}
+		_putchar(c);
+	}
+	_putchar('\n');
+}
