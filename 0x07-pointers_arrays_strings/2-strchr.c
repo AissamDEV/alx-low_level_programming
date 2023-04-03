18ffb42f05f25cb8359d81659aa1f8b2e3d2762f
aissam <aissam@gmail.com>
2023-04-03 10:05:46 -0700

    strchr,p

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <string.h>
+/**
+  *_strchr- function that locates a character in a string
+  *@s: string
+  *@c: first occurrence of the character
+  *Return: result
+  */
+char *_strchr(char *s, char c)
+{
+	char *result = strchr(s, c);
+
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		return (result);
+	}
+}
