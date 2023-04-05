cc9977bcf49964c8384871bd69bdf112898ed29e
aissam <aissam@gmail.com>
2023-04-05 09:02:36 -0700

    factorial

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,8 +1,10 @@
 #include <string.h>
 /**
-  *
-  *
-  *
+  *factorial- function that return
+  *the factorial of a given number
+  *@n: number to be calucate
+  *Return: -1 if n less than 0
+  *and fact if more than 0
   */
 int factorial(int n)
 {
