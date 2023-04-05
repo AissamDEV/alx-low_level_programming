ad74f186fcc6de0350357fa921574c122f0a0bfb
aissam <aissam@gmail.com>
2023-04-05 16:48:19 -0700

    is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,17 +1,31 @@
 #include <math.h>
 /**
+  *is_divisible - helper function to check
+  *		if a number is divisible by any
+  *@n: the number to check for divisibility
+  *@i: the integer to check divisibility
+  *Return: 1 if the number is divisible by i, 0 otherwise
+  */
+int is_divisible(int n, int i)
+{
+	if (i == 1)
+		return (0);
+	else if (n % i == 0)
+		return (1);
+	else
+		return (is_divisible(n, i - 1));
+}
+/**
   *is_prime_number - function that check prime numbers
   *@n: number to be check
   *Return: 0 if not prime and 1 otherwise return
   */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 		return (0);
-	for (i = 2; i <= n / 2; i++)
-		if (n % i == 0)
-			return (0);
-	return (1);
+	else if (n == 2)
+		return (1);
+	else
+		return (!is_divisible(n, n - 1));
 }
