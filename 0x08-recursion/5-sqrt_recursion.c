e9bea48575c66e7688aca225d48c8a499e20fda0
aissam <aissam@gmail.com>
2023-04-05 14:50:40 -0700

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,10 +1,11 @@
 #include <math.h>
 #include "main.h"
 /**
-  *
-  *
-  *
-  *
+  *_sqrt_helper - function thats finding sqrt
+  *@n: number for which you want to find the square root
+  *@c: the current guess for the square root
+  *Return: 1 if n is 1 and -1 if n not a real number
+  *c if c is the square root of n
   */
 int _sqrt_helper(int n, int c)
 {
@@ -22,6 +23,12 @@ int _sqrt_helper(int n, int c)
 	}
 	return (_sqrt_helper(n, c + 1));
 }
+/**
+  *_sqrt_recursion - function that returns
+  *the natural square root of a number
+  *@n: number
+  *Return: square root
+  */
 int _sqrt_recursion(int n)
 {
 	return (_sqrt_helper(n, 1));
