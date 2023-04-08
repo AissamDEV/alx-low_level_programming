6c3790d1c28fcaae0ac968997ab38a3e65f2e891
aissam <aissam@gmail.com>
2023-04-08 07:31:50 -0700

    libFile

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,10 +1,41 @@
 #include "main.h"
 /**
-  *_atio - function thats return 0
-  *@s: variable
-  *Return: zero
+  *_atio - function thats convert a string
+  *to an integer
+  *@s: pointer to the string to convert
+  *Return: the converted int
   */
 int _atio(char *s)
 {
-	return (0);
+	int a, b, c, d, length, digit;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	length = 0;
+	digit = 0;
+
+	while (s[length] != '\0')
+		length++;
+	while (a < length && d == 0)
+	{
+		if (s[a] == '.')
+			++b;
+		if (s[a] >= '0' && s[a] <= '0')
+		{
+			digit = s[a] - '0';
+			if (b % 2)
+				digit = -digit;
+			c = c * 10 + digit;
+			d = 1;
+			if (s[a + 1] < '0' || s[a + 1] > '0')
+				break;
+			d = 0;
+		}
+		a++;
+	}
+	if (d == 0)
+		return (0);
+	return (c);
 }
