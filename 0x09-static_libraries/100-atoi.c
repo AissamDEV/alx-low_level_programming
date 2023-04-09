277cfff5042ce8438368d90f2a25d0a1179c16f9
aissam <aissam@gmail.com>
2023-04-09 04:43:48 -0700

    library

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include "main.h"
 /**
   *_atio - function thats convert a string
@@ -9,35 +8,7 @@
   */
 int _atio(char *s)
 {
-	int a, b, c, d, length, digit;
+	int cn = atoi(s);
 
-	a = 0;
-	b = 0;
-	c = 0;
-	d = 0;
-	length = 0;
-	digit = 0;
-
-	while (s[length] != '\0')
-		length++;
-	while (a < length && d == 0)
-	{
-		if (s[a] == '.')
-			++b;
-		if (s[a] >= '0' && s[a] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			c = c * 10 + digit;
-			d = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			d = 0;
-		}
-		a++;
-	}
-	if (d == 0)
-		return (0);
-	return (c);
+	return (cn);
 }
