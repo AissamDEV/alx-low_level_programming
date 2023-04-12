9b307ada22d36dfbf1516999cdbdd95dabcebe77
aissam <aissam@gmail.com>
2023-04-11 17:07:29 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height; i++)
 	{
-		array[i] = calloc(width, sizeof(int));
+		array[i] = malloc(width * sizeof(int));
 		if (array[i] == NULL)
 		{
 			for (j = i - 1; j >= 0; j--)
