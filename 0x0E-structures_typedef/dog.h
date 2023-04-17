298f40620a8f0efd9af1cd23147a0ee02611121b
aissam <aissam@gmail.com>
2023-04-17 04:26:59 -0700

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,11 @@
-struct dog{
+/**
+  *dog - name of the struct
+  *@name: name of the dog
+  *@age: ege of the dog
+  *@owner: the owner of the dog
+  */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
