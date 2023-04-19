db1d29e3f64a1422c8ec271c690f7c87ccc61486
aissam <aissam@gmail.com>
2023-04-19 10:07:52 -0700

    header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-typedef void (*print_function)(char *);
-void print_name(char *name, print_function f);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif /* FUNCTION_POINTERS_H */
