86af4000c3238fc1f797343c9ad6cb7e3a4b1b8e
aissam <aissam@gmail.com>
2023-04-19 13:24:54 -0700

    array interator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -10,9 +10,13 @@
   */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	size_t i;
-	for (i = 0; i < size; i++)
+	int *ptr = array + size + 1;
+
+	if (array && size && action)
 	{
-		action(array[i]);
+		while (array <= ptr)
+		{
+			action(*array++);
+		}
 	}
 }
