6a17aeab9a9f29097ecdefd57755d4eddc75c692
aissam <aissam@gmail.com>
2023-04-19 05:28:50 -0700

    function pointers

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,6 +2,6 @@
 #define FUNCTION_POINTERS_H
 
 typedef void (*print_function)(char *);
-void print_name(char *name, void (*f)(char *));
+void print_name(char *name, print_function f);
 
 #endif /* FUNCTION_POINTERS_H */
