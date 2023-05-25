bc2039fa07e836f18bd0d69e49d77779ca541af2
AissamDEV <aissamaboukassim@gmail.com>
2023-05-25 06:29:20 -0700

    is plindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -9,9 +9,9 @@
 int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
-		return 0;
+		return (0);
 
-	return 1 + _strlen_recursion(s + 1);
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -25,12 +25,12 @@ int _strlen_recursion(char *s)
 int is_palindrome_helper(char *s, int start, int end)
 {
 	if (start >= end)
-		return 1;
+		return (1);
 
 	if (s[start] == s[end])
-		return is_palindrome_helper(s, start + 1, end - 1);
+		return (is_palindrome_helper(s, start + 1, end - 1));
 
-	return 0;
+	return (0);
 }
 
 /**
@@ -42,5 +42,6 @@ int is_palindrome_helper(char *s, int start, int end)
 int is_palindrome(char *s)
 {
 	int len = _strlen_recursion(s);
-	return is_palindrome_helper(s, 0, len - 1);
+
+	return (is_palindrome_helper(s, 0, len - 1));
 }
